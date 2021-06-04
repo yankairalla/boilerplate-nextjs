78b4ec5a83c99999616fa6c11dcdd024423b6f3b
Yan Kairalla <yanmkairalla@hotmail.com>
2021-06-04 16:59:30 -0300

    finished boilerplate

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado -- Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starte to work with Styled Components"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092b" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+export default App
